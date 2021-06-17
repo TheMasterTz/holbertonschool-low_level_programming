3eb7f918621df5900eb24daf048bee1ee18eab28
TheMasterTz <palavras2001@hotmail.com>
2021-06-17 14:06:18 -0700

    ejercicio_6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Char_;
+int Int_;
+long int LongInt_;
+long long int LongsInt_;
+float Float_;
+  
+printf("Size of a char: %Id byte(s)\n", sizeof(Char_))
+printf("Size of a int: %Id byte(s)\n", sizeof(Int_))
+printf("Size of a long in: %Id byte(s)\n", sizeof(LongInt_))
+printf("Size of a long long int: %Id byte(s)\n", sizeof(LongsInt_))
+printf("Size of a float: %Id byte(s)", sizeof(Float_))
+}
