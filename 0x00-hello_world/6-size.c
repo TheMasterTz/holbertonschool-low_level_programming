ff5868e7191f8b507c281df050f1ae3b4aacfbad
TheMasterTz <palavras2001@hotmail.com>
2021-06-17 14:17:41 -0700

    ejercicio_6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-printf("Size of a char: %Id byte(s)\n", sizeof(char));
-printf("Size of a int: %Id byte(s)\n", sizeof(int));
-printf("Size of a long int: %Id byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %Id byte(s)\n", sizeof(long long int));
-printf("Size of a float: %Id byte(s)", sizeof(float));
-return (0);
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+
+	return (0);
 }
