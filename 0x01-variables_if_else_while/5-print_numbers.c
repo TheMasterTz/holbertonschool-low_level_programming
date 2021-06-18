7bf7469e1f25cb3832f0e9534756617377c64bf9
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 16:46:30 -0700

    ejercicio_5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int NUM;
+
+	for (NUM = 0; NUM <= 9; ++NUM)
+	{
+		printf("%d", NUM);
+	}
+	printf("\n");
+
+	return (0);
+}
