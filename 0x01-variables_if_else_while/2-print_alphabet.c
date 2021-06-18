4e9d6fc2aa888086a5cb8596ac7d30168162dc51
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 14:52:20 -0700

    ejercicio_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char cont;
+
+	for (cont = 'a'; cont <= 'z'; cont++)
+
+		putchar(cont);
+		putchar('\n');
+
+		return (0);
+}
