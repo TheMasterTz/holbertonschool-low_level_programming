9a6d17784ba08991f11520845b154068e5a70402
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 20:37:34 -0700

    ejercicio_10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        int q = 48;
+        int w = 49;
+
+        while (q < 58)
+        {
+                putchar(q);
+                putchar(w);
+
+                if (q == 56 && w == 57)
+                {
+                        putchar('\n');
+                        q++;
+                        w++;
+                }
+                else
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
+                if (w < 57)
+                {
+                        w++;
+                }
+                else
+                {
+                        q++;
+                        w = q + 1;
+                }
+        }
+        return (0);
+}
