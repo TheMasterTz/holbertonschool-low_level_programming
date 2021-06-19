e6330cb4fad9ac3277a69c55d28763daa0f0acb2
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 17:27:39 -0700

    ejercicio_7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char chart;
+
+	for (chart = 'z';chart >= 'a'; chart--)
+	{
+		putchar(chart);
+	}
+	putchar('\n');
+
+	return (0);
+}
