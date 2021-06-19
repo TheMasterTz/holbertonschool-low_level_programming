22883794a6ccd60ef5e6441a10c1ee5c1b36608f
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 18:13:18 -0700

    ejercicio_9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int af;
+
+	for (af = 48; af < 58; af++)
+	{
+		putchar(af);
+
+		if (af != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		
+		else if (af == 57)
+		{
+			putchar(36);
+		}
+	
+	}
+	putchar('\n');
+
+	return (0);
+}
+
