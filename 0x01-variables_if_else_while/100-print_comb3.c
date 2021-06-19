e1bc045f2b859e7a3e3df7217132c0548596b370
TheMasterTz <palavras2001@hotmail.com>
2021-06-18 20:49:11 -0700

    ejercicio_10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,34 +7,35 @@
 */
 int main(void)
 {
-        int q = 48;
-        int w = 49;
+	int q = 48;
+	int w = 49;
 
-        while (q < 58)
-        {
-                putchar(q);
-                putchar(w);
+	while (q < 58)
+	{
+		putchar(q);
+		putchar(w);
 
-                if (q == 56 && w == 57)
-                {
-                        putchar('\n');
-                        q++;
-                        w++;
-                }
-                else
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-                if (w < 57)
-                {
-                        w++;
-                }
-                else
-                {
-                        q++;
-                        w = q + 1;
-                }
-        }
-        return (0);
+		if (q == 56 && w == 57)
+		{
+			putchar('\n');
+			q++;
+			w++;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		if (w < 57)
+		{
+			w++;
+		}
+		else
+		{
+			q++;
+			w = q + 1;
+		}
+	}
+
+	return (0);
 }
