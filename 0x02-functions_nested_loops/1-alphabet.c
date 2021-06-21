c61b1edfc79108591ab4cefc3049c739a8d8c20e
TheMasterTz <palavras2001@hotmail.com>
2021-06-21 14:50:44 -0700

    ejercicio_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton."
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char vr;
+
+	for (vr = 'a'; vr <= 'z'; vr++)
+	{
+		_putchar(vr);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
