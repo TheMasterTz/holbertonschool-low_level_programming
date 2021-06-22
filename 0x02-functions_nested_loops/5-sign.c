3347ebd59556613fbb3ccf26100d11a61ca845be
TheMasterTz <palavras2001@hotmail.com>
2021-06-22 08:46:27 -0700

    ejercicio_5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - function that checks for alphabetic character.
+ * @c: The input character to check
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(45);
+                return (-1);
+	}
+
+	else 
+		_putchar(48);
+                return (0);
+}
