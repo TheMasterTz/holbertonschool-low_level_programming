7fa2275ef855aadf049f1e3d94cfc63c7aba808e
TheMasterTz <palavras2001@hotmail.com>
2021-06-22 11:02:42 -0700

    ejercicio_7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 /**
  * print_last_digit - function that prints the last digit of a number.
  * @x: The input number to check
- * 
+ *
  * Return: int.
  */
 
 int print_last_digit(int x)
 {
 	x %= 10;
-	
+
 	if (x < 0)
 	{
 		x *= (-1);
