960d3d3969715e0424828cef604b26b99185e301
TheMasterTz <palavras2001@hotmail.com>
2021-06-22 08:17:43 -0700

    ejercicio_4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+
+	}
+}
