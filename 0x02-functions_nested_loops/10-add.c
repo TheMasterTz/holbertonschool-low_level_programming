c8c5998b43181539f47d2cbfe07c029393d0f66c
TheMasterTz <palavras2001@hotmail.com>
2021-06-22 13:25:27 -0700

    ejercicio_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @n1: input integer 1
+ * @n2: input integer 2
+ * Return: int.
+ */
+
+int add(int N1, int N2)
+{
+	int total;
+
+	total = N1 + N2;
+	return (total);
+}
