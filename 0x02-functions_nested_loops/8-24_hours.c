ed15424db3878ec7596881e2b6713afe0c2c722f
TheMasterTz <palavras2001@hotmail.com>
2021-06-22 12:27:00 -0700

    ejercicio_8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - function with no argument
+ *
+ * Description: Using for loops to print every minute in 24hr
+ * Return: none
+ */
+
+void jack_bauer(void)
+{
+	int hora;
+	int minutos;
+
+	for (hora = 0; hora < 24; hora++)
+	{
+		for (minutos = 0; minutos < 60; minutos++)
+		{
+			_putchar(hora / 10 + '0');
+			_putchar(hora % 10 + '0');
+
+			_putchar(':');
+
+			_putchar(minutos / 10 + '0');
+			_putchar(minutos % 10 + '0');
+
+			_putchar('\n')
+		}
+	}
+}
+
