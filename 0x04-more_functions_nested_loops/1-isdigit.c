a5994e96abe2a04d998534d48e4254591cf7709c
TheMasterTz <palavras2001@hotmail.com>
2021-06-24 13:30:40 -0700

    ejercicio_1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
-
 /**
  *_isdigit - checks for a digit
  *@c:int
  *Return: 1 if digit, 0 otherwise
  */
+
 int _isdigit(int c)
 {
-	int num;
-
-	for (num = '0'; num < 10; num++)
+	char num1;
+	
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		if (c == num)
-		{
+		if (num1 == c)
 			return (1);
-		}
 	}
 	return (0);
 }
