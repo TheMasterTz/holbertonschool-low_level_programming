c07bb1e29e0624521ea898a3006f4e1867194092
TheMasterTz <palavras2001@hotmail.com>
2021-06-24 16:01:31 -0700

    ejercicio_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x < 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0));
 		{
@@ -28,6 +28,11 @@ int main(void)
 		else
 		{
 			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
