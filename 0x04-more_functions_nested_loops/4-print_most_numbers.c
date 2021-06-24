3683faeb3b6a107bac772173d9ae265e13e5fc6e
TheMasterTz <palavras2001@hotmail.com>
2021-06-24 13:48:36 -0700

    ejercicio_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,14 @@
 
 void print_most_numbers(void)
 {
-	for (int N = 0; N < 10; N++)
+	int N;
+
+	for (N = 0; N < 10; N++)
 	{
 		if (N != 2 && N != 4)
 		{
-			_putchar(N + '0')
+			_putchar(N + '0');
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
