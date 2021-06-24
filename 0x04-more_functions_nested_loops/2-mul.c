b177ef06b5d6d74413e53fd7b182e08ff0596474
TheMasterTz <palavras2001@hotmail.com>
2021-06-24 10:02:41 -0700

    ejercicio_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *mul - multiply 2 numbers
+ *@a: int
+ *@b: int
+ *Return: product
+ */
+
+int mul(int a, int b)
+{
+	int resultado;
+
+	resultado = a * b;
+
+	return (resultado);
+
+}
