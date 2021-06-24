bbb131ec8ac79b2530072165f39881b259edb449
TheMasterTz <palavras2001@hotmail.com>
2021-06-24 16:04:02 -0700

    ejercicio_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - Prints a Fizz Buzz program
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Prints a Fizz Buzz program
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0));
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		if else (x % 3 == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		if else (x % 5 == 0)
+		else if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
@@ -35,6 +34,7 @@ int main(void)
 			printf(" ");
 		}
 	}
+
 	printf("\n");
 	return (0);
 }
