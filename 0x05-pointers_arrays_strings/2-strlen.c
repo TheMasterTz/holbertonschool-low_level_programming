8e89a4f42afb4e2a454c7a638a53354f017ec750
TheMasterTz <palavras2001@hotmail.com>
2021-06-28 12:50:45 -0700

    ejercicio_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c
+
+	for (c = 0; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
