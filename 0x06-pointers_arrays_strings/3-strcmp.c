a141d3057654f0b2269fc552201cb73cff7beb65
TheMasterTz <palavras2001@hotmail.com>
2021-07-02 14:34:01 -0700

    ejercicio3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (s1[] == s2[])
+	while (*s1 == *s2)
 	{
 		s1++;
 		s2++;
 
-		if (s1[] == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
 	}
 
-	i = s1[] - s2[];
+	i = *s1 - *s2;
 
 	return (i);
 }
