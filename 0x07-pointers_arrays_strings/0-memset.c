01ebb5ac45ee3ee985f3b99b02663afaecbc6088
TheMasterTz <palavras2001@hotmail.com>
2021-07-05 12:28:52 -0700

    ejercicio-0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * _memset - fills memory with a constant byte
+  * @s: char
+  * @b: char
+  * @n: unsigned int
+  * Return: char*
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
