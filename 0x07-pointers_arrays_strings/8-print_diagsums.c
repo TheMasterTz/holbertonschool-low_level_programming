fc806e95151b59ce1ffb6489610b1a6e2039fbe3
TheMasterTz <palavras2001@hotmail.com>
2021-07-06 13:25:39 -0700

    ejercicio-7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,19 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
+	int x;
 	int y;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i < (size * size); i = i + (size + 1))
-	{	sum1 = sum1 + *(a + i);
+	for (x = 0; x < (size * size); x = x + (size + 1))
+	{
+		sum1 = sum1 + *(a + x);
 	}
 	for (y = (size - 1); y <= (size * (size - 1)); y = y + (size - 1))
 	{
 		sum2 = sum2 + *(a + y);
 	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
