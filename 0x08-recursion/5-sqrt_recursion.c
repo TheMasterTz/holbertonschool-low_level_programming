f29eff515993be5679ad02942d2b54ce6b7c84b9
TheMasterTz <palavras2001@hotmail.com>
2021-07-07 13:37:14 -0700

    ejercicio-5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+int _sqrt(int n, int i);
+/**
+ *_sqrt_recursion - returns natural square root
+ *@n: int
+ *@i: int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (_sgrt(n, 1));
+	}
+}
+
+/**
+ * sqrt - compare squares with numbers
+ *@n:int
+ *@i:int
+ *Return: int
+ */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+	
+	if (sqrt == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
