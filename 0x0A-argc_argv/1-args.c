1703e37822c4309d05755bceeb1f3820953d32bd
TheMasterTz <palavras2001@hotmail.com>
2021-07-09 14:47:16 -0700

    ejercicio1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - prints # of args
  * @argc: int
  * @argv: char*
  * Return: Always 0
  */
-int main (int argc, char *argv[])
-{
-	argc++;
-	if (argc >= 0)
-		printf("%s\n", argc - 1);
 
-	return (0);
+int main(int argc, char *argv[])
+{
+	argv++;
+		if (argc >= 0)
+				printf("%d\n", argc - 1);
+
+		return (0);
 }
