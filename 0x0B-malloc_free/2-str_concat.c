284ec2b74682f3eadb42fbb013f82a9eae5f2bd2
TheMasterTz <palavras2001@hotmail.com>
2021-07-14 14:50:33 -0700

    ejercicio2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,44 +9,45 @@
  *
  * Return: the two strings concatenated
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, l = 0, r = 0;
-	char *s;
+		int i = 0, j = 0, k = 0, l = 0;
+		char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+		if (s1 == NULL)
+			s1 = "";
 
-	while (s1[i])
-	{
-		i++;
-	}
-	while (s2[j])
-	{
-		j++;
-	}
+		if (s2 == NULL)
+			s2 = "";
 
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
+		while (s1[i])
+			i++;
 
-	if (s == NULL)
-		return (NULL);
-
-	while (r < l)
-	{
-		if (r <= i)
-			s[r] = s1[r];
-
-		if (r >= i)
-		{
-			s[r] = s2[j];
+		while (s2[j])
 			j++;
-		}
-		r++;
-	}
 
-	s[r] = '\0';
+		l = i + j;
+		s = malloc((sizeof(char) * l) + 1);
+
+		if (s == NULL)
+			return (NULL);
+
+		j = 0;
+
+		while (k < l)
+		{
+			if (k <= i)
+				s[k] = s1[k];
+
+			if (k >= i)
+			{
+				s[k] = s2[j];
+				j++;
+			}
+			k++;
+		}
+
+	s[k] = '\0';
 	return (s);
 }
