13c29dd70ccb5d3f79f7175a0442f1e46d6810fc
TheMasterTz <palavras2001@hotmail.com>
2021-07-19 13:26:48 -0700

    ejercicio-2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Prints the file name of the program
+ * main - Prints the name of the file of the program
+ * was compiled from, followed by a new line.
  *
- * Retrun: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
