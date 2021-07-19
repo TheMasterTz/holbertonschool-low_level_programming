d3d251ba863ba08bb3353b63418e012a74dcc09d
TheMasterTz <palavras2001@hotmail.com>
2021-07-19 15:22:28 -0700

    ejercicio-1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "funciones.h"
+
+/**
+ * init_dog - initialize a dog structure
+ *
+ * @d: A dog structure
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d -> name = name;
+		d -> owner = owner;
+		d -> age = age;
+	}
+}
