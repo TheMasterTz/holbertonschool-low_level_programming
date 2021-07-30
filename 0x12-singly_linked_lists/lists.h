1183de255930b13fd7185cf4151e282c193f8943
TheMasterTz <palavras2001@hotmail.com>
2021-07-30 12:21:06 -0700

    libreria

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,8 @@
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,15 +10,17 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
-    unsigned int len;
-    struct list_s *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-size_t print_listint(const list_t *h);
+int _strlen(const char *s);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
