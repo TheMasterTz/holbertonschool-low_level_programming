0627420ac3bab68588e14a7892049da2731ee8ac
TheMasterTz <palavras2001@hotmail.com>
2021-07-30 15:37:32 -0700

    ejercicio 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (head != NULL && str != NULL)
 	{
 		list = malloc(sizeof(list_t));
-		if (temp == NULL)
+		if (list == NULL)
 			return (NULL);
 
 		list->str = strdup(str);
