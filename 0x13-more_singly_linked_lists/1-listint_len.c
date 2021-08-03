081f4ac41e826b9f57931498897d09f0d48b3081
TheMasterTz <palavras2001@hotmail.com>
2021-08-03 08:34:10 -0700

    correccion

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the head
+ * Return: length of the list
+ *
+ */
 size_t listint_len(const listint_t *h)
 {
 	int iter = 0;
