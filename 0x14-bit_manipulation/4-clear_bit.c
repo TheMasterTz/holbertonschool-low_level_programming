1b1815f5e0ecf59ec471e15828bce8d53c55b9bd
TheMasterTz <palavras2001@hotmail.com>
2021-08-05 16:15:49 -0700

    ejercicio 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *set_bit - set the value of a bit to 1
+ *@n: unsigned long int*
+ *@index: unsigned int
+ *Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+	
+	m = 1 << index;
+	*n = *n & ~m;
+	return (1);
+}
