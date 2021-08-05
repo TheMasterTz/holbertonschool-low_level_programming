afd955c252ee6edcb615a041c8a55f1c15ac030e
TheMasterTz <palavras2001@hotmail.com>
2021-08-05 14:53:19 -0700

    ejercicio 1 + Readme

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: const char *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int count = 0;
+	unsigned int i;
+	unsigned int dec = 0;
+	unsigned int base = 1;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		count++;
+		b++;
+	}
+
+	b--;
+
+	for (i = 0; i < count; i++, b--)
+	{
+ 		if (*b == '1')
+ 			dec += base;
+
+		else if (*b != '0' && *b != '1')
+			return (0);
+
+		base *= 2;
+	}
+
+	return (dec);
+}
