46b6fbb5866933397655589c3787693a1571a1c6
TheMasterTz <palavras2001@hotmail.com>
2021-08-09 13:01:07 -0700

    ejercicio 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf | !filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
