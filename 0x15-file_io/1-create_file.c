5d0c2ec68cf7e3846422eeb26e927f586fce8e7f
TheMasterTz <palavras2001@hotmail.com>
2021-08-09 14:11:06 -0700

    ejercicio 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		while (text_content[count])
