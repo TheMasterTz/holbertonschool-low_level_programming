7a1d20ce4bff30a5b338770a03672f0c103190d2
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-17 19:57:19 -0500

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- * dlistint_t *insert_dnodeint_at_index - insert node in given position
- * @head: double pointer
- * @idx: index
- * @n: value of nodes
- *
- * Return: the address of the new node created, od NULL if it failed.
- */
- dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
- {
-	dlistint_t *current = NULL, *Newnodo = NULL;
-	unsigned int length = 0, i = 0;
+  * insert_dnodeint_at_index - Inserts a new node at a given position
+  * @h: The head of the doubly linked list
+  * @idx: The index in which insert the new node
+  * @n: The number to insert in the new node
+  *
+  * Return: The address of the new node, or NULL if it failed
+  */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = NULL, *new_node = NULL;
+	unsigned int i = 0, length = 0;
 
 	if (h == NULL)
 		return (NULL);
+
 	if (*h == NULL && idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -28,49 +30,57 @@
 	{
 		if (i == idx)
 		{
-			Newnodo = createNode(n, current, current->prev);
-			current->prev = Newnodo;
-			current = Newnodo;
-			current->prev->next = Newnodo;
-			return (Newnodo);
+			new_node = create_node(n, current, current->prev);
+			current->prev = new_node;
+			current = new_node;
+			current->prev->next = new_node;
+			return (new_node);
 		}
+
 		current = current->next;
 		++i;
 	}
+
 	return (current);
 }
+
 /**
- * print_dlistint - print all list elements
- * @h: pointer to head
- *
- * Return: Always EXIT_SUCCESS.
- */
+  * dlistint_len - Counts the number of elements in a doubly linked list
+  * @h: The double linked list to count
+  *
+  * Return: Number of elements in the doubly linked list
+  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int length;
+	int lenght = 0;
 
-	for (length = 0; h != NULL; length++)
+	while (h != NULL)
 	{
+		++lenght;
 		h = h->next;
 	}
-	return (length);
+
+	return (lenght);
 }
+
 /**
- * createNode - create a new node with values
- * @n: the number of the new node
- * @next: the next node of the new node
- * @prev: the previous node of the new node
- *
- * Return: the address of the new node created
- */
-dlistint_t *createNode(unsigned int n, void *next, void *prev)
+  * create_node - Create a new node with values
+  * @n: The number of the new node
+  * @next: The next node of the new node
+  * @prev: The previous node of the new node
+  *
+  * Return: The address of the new node created
+  */
+dlistint_t *create_node(unsigned int n, void *next, void *prev)
 {
-	dlistint_t *Newnodo = malloc(sizeof(dlistint_t));
-	if (Newnodo == NULL)
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	
-	Newnodo->n = n;
-	Newnodo->next = next;
-	Newnodo->prev = prev;
-	return (Newnodo);
+
+	new_node->n = n;
+	new_node->next = next;
+	new_node->prev = prev;
+	return (new_node);
 }
